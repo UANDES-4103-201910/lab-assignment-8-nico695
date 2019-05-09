6bea13f0f1714ad45fabf82494b52578e7919024
Nicolas <nmgrigoriewgridensol@miuandes.cl>
2019-05-09 13:32:59 -0400

    Pantalla

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,27 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+function print() {
+	$('.key').click(function(){
+		console.log($(this).text())
+		
+	})
+}
+
+$(document).ready(function(){
+	print();
+	var writea = $('#ta');
+
+	$('.key').click(function(){
+
+		var keya = $(this).text();
+
+		var b = document.getElementById('write').innerHTML += keya;
+		
+	})
+	
+
+  $('#1').click(function(){
+    $('.key').toggle();
+  });
+
 });
